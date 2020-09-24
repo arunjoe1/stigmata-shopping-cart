16dde771826d89b2357c994c1cd12cff099a6e95
arunjoe1 <arunjs46@gmail.com>
2020-09-24 16:23:29 +0530

    add redux products

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Cart from './components/Cart';
 import Filter from './components/Filter';
 import Products from './components/Products';
 import data from "./data.json";
+import store from './store';
+import { Provider } from "react-redux";
 
 
 
@@ -13,7 +15,7 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
-      cartItems :localStorage.getItem("cartItems")?JSON.parse(localStorage.getItem("cartItems")):[],
+      cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
       size: "",
       sort: ""
     }
@@ -25,27 +27,28 @@ class App extends React.Component {
   }
   removeFromCart = (product) => {
     const cartItems = this.state.cartItems.slice();
-    this.setState({cartItems:cartItems.filter(x=>x._id !== product._id ),
-    
+    this.setState({
+      cartItems: cartItems.filter(x => x._id !== product._id),
+
     });
-    localStorage.setItem("cartItems" , JSON.stringify(cartItems.filter(x=>x._id !== product._id )));
-    
+    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter(x => x._id !== product._id)));
+
   };
 
   addToCart = (product) => {
     const cartItems = this.state.cartItems.slice();
     let alreadyInCart = false;
     cartItems.forEach(item => {
-      if(item._id === product._id){
+      if (item._id === product._id) {
         item.count++;
-        alreadyInCart= true;
+        alreadyInCart = true;
       }
     });
-    if(!alreadyInCart){
-      cartItems.push({...product , count:1})
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 })
     }
-    this.setState({cartItems});
-    localStorage.setItem("cartItems" , JSON.stringify(cartItems));
+    this.setState({ cartItems });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }
 
   sortProducts = (event) => {
@@ -56,7 +59,7 @@ class App extends React.Component {
       sort: sort,
       products: this.state.products
         .slice()
-        .sort((a, b) => 
+        .sort((a, b) =>
           sort === "lowest" ?
             a.price > b.price ? 1 : -1 :
             sort === "highest" ?
@@ -83,42 +86,45 @@ class App extends React.Component {
     }
   }
   render() {
+
     return (
-      <div className="grid-container">
-        <header>
-          <a href="/">Stigmata Shopping Cart</a>
-        </header>
+      <Provider store={store}>
+        <div className="grid-container">
+          <header>
+            <a href="/">Stigmata Shopping Cart</a>
+          </header>
 
-        <main>
-          <div className="content">
-            <div className="main">
-              <Filter count={this.state.products.length}
+          <main>
+            <div className="content">
+              <div className="main">
+                <Filter count={this.state.products.length}
 
-                size={this.state.size}
-                sort={this.state.sort}
-                filterProducts={this.filterProducts}
-                sortProducts={this.sortProducts}
+                  size={this.state.size}
+                  sort={this.state.sort}
+                  filterProducts={this.filterProducts}
+                  sortProducts={this.sortProducts}
 
-              />
-              <Products products={this.state.products} addToCart={this.addToCart} />
+                />
+                <Products products={this.state.products} addToCart={this.addToCart} />
+              </div>
+              <div className="sidebar">
+                <Cart
+                  cartItems={this.state.cartItems}
+                  removeFromCart={this.removeFromCart}
+                  createOrder={this.createOrder}
+
+                />
+              </div>
+
             </div>
-            <div className="sidebar">
-              <Cart 
-              cartItems={this.state.cartItems} 
-              removeFromCart={this.removeFromCart} 
-              createOrder={this.createOrder}
-              
-              />
-            </div>
-
-          </div>
-        </main>
-        <footer>
-          All rights Reserved.
+          </main>
+          <footer>
+            All rights Reserved.
         </footer>
 
 
-      </div>
+        </div>
+      </Provider>
     );
 
   }
